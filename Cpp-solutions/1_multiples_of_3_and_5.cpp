d2be27b03a51fb659456ec7325dd4a94a853b937
chinhwee <ongchinhwee@gmail.com>
2018-08-30 14:45:14 +0800

    Added problem explanation

--- Cpp-solutions/1_multiples_of_3_and_5.cpp
+++ Cpp-solutions/1_multiples_of_3_and_5.cpp
@@ -6,6 +6,25 @@
 
 using namespace std;
 
+/*****************************************************************************************/
+// Project Euler #1: Multiples of 3 and 5
+// If we list all the natural numbers below 10 that are multiples of 3 or 5,
+// we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//
+// Find the sum of all the multiples of 3 or 5 below N.
+//
+// Constraints:
+// T: number of test cases
+// 1 <= T <= 10^5
+// 1 <= N <= 10^9
+//
+// We could make use of Sum of Arithmetic Progression to determine the sum of multiples of
+// 3, 5 and 15 (multiples of 3 and 5) below N.
+//
+// Sum of multiples of 3 or 5 
+// = Sum of multiples of 3 + Sum of multiples of 5 - Sum of multiples of 15
+/******************************************************************************************/
+
 unsigned long int sum_multiples(unsigned long int x) {
     
     unsigned long int sum_3, sum_5, sum_15;
